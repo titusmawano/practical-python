5ac699f1daddc32ada6c8a659c7b2ee45bcd8d44
Tito <tito@Titos-MacBook-Pro.local>
2020-07-01 01:41:22 +0300

    .

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+for i in range(1,11):
+    height = round(0.6 * height, 4)
+    print(height)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,25 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
+def portfolio_cost(filename):
+    cost = 0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            r = line.split(',')
+            try:
+                row = ( r[0], int(r[1]), float(r[2]) )
+                cost = cost + row[1] * row[2]
+            except ValueError:
+                print("nada")
+    return cost
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(cost)
